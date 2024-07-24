498a4dfa3c979fb476f1dfbf6b828044ac889c05
amarnath <amarnathzero8@gmail.com>
2024-07-25 00:29:32 +0530

    vpc sg bs

---
+++ 03-bastion/main.tf
@@ -0,0 +1,14 @@
+module "bastion" {
+    source = "terraform-aws-modules/ec2-instance/aws"          ##module from open source
+    name = "${var.project_name}-${var.environment}-bastion"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
+    subnet_id = local.public_subnet_id
+    ami = data.aws_ami.ami_info.id
+    tags = merge(
+        var.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}-bastion"
+        }
+    )
+}
